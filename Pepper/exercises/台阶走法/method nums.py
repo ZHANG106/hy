f783d39a902a2ad088f55676a7fa6e3a9eea20f1
David <zyq694463312@gmail.com>
2019-10-13 23:03:51 +0800

    new one

--- Pepper/exercises/台阶走法/method nums.py
+++ Pepper/exercises/台阶走法/method nums.py
@@ -1,3 +1,20 @@
 class Solution:
+    '''
+    新建一个类，将所有的解题的方法封装在里面
+    '''
     def method_nums(self, nums: int) -> int:
-        pass
+        '''
+        解题的方法
+        :param nums: 输入值是总台阶数
+        :return: 输出值是走法
+        '''
+        res = 0
+
+        return res
+
+
+if __name__ == '__main__':
+    test_ex = input()
+    eg = Solution()
+    answer = eg.method_nums(test_ex)
+    print(answer)
