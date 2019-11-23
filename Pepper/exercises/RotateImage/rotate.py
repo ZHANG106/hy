9b0d50304cab477a9d7630442ac9f90084a4a6ea
zhang <zyq694463312@gmail.com>
2019-11-24 01:21:55 +0800

    update

--- Pepper/exercises/RotateImage/rotate.py
+++ Pepper/exercises/RotateImage/rotate.py
@@ -1,5 +1,11 @@
-import numpy as np
-
-
 def rotate(matrix):
-    return np.fliplr(matrix.T)
+    for i in range(len(matrix)-1):
+        for j in range(i+1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        matrix[i].reverse()
+    matrix[-1].reverse()
+    return matrix
+
+
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+print(rotate(matrix))
